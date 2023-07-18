7c20cf83cf39f1bd23698020dc8927fbced1fa3a
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-18 23:40:27 +0800

    laravel 8 - lesson 8

---
+++ blog/resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="/app.css" />
+        <link rel="stylesheet" href="/posts.css" />
+    </head>
+
+    <body class="posts">
+        <article>
+            <h2><a href="/posts/my-first-post">My First Post</a></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus. Ut sed elit at ligula tempor porta sed odio.
+        </article>
+        <article>
+            <h2><a href="/posts/my-second-post">My Second Post</a></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus. Ut sed elit at ligula tempor porta sed odio.
+        </article>
+        <article>
+            <h2><a href="/posts/my-third-post">My Third Post</a></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus. Ut sed elit at ligula tempor porta sed odio.
+        </article>
+    </body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', function () {
+    return view('posts');
+});
+
+Route::get('/posts/{post}', function ($slug) {
+    $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
+
+    return view('post', ['post' => $post]);
+});
