752c164113f06b7b5ff3df2956bcbf6a927a5d50
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-19 11:28:57 +0300

    laravel app

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -8,21 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/posts', function () {
-    return view('posts');
-});
-
-Route::get('/posts/{post}', function ($slug) {
-    $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
-
-    return view('post', ['post' => $post]);
 });
