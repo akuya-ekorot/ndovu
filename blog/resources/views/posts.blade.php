85c19c66eeafeb6ccaaed2256d89216a01bcd3cd
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-19 14:48:51 +0300

    post model;

---
+++ blog/resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Posts</title>
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/css/posts.css">
+    </head>
+
+    <body>
+        <h1>Posts</h1>
+        <article>
+            <h2><a href="/posts/my-first-post">My First Post</a></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </article>
+        <article>
+            <h2><a href="/posts/my-second-post">My Second Post</a></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </article>
+        <article>
+            <h2><a href="/posts/my-third-post">My Third Post</a></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </article>
+    </body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
+
+Route::get('/posts/{post}', function ($slug) {
+
+    return view('post', [
+        'post' => Post::find($slug),
+    ]);
+
+})->where('post', '[A-z_-]+');
